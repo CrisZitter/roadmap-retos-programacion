a0fb299d338467bd8974fc3621f7818fbefb412b
Cristian Andres Zitterkopf <128307785+CrisZitter@users.noreply.github.com>
2024-06-21 09:35:58 -0300

    Create CristianAndresZitterkopf.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CristianAndresZitterkopf.py
@@ -0,0 +1,28 @@
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ ###www.python.org
+'''se pueden escribir
+comentarios en
+varias lineas con comillas simples'''
+
+"""o tambien se pueden 
+con comillas dobles """
+
+variable="esto es una variable"
+
+##Tipos de datos
+
+cadena_de_texto="Esto es una cadena de texto"
+flotante=3.25
+booleano=True
+enteros=4
+
+print("Hola Python"
+ 
+ 
